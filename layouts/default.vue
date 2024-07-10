4ca3f6024db4c1976210c39a96ffb8524fc1273d
Victor Georges <victorgeorges54@gmail.com>
2024-07-10 21:06:57 +0200

    build: nuxt first app with some test pages

---
+++ layouts/default.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <header>
+            <nav>
+                <NuxtLink to="/"></NuxtLink>
+                <ul>
+                    <li><NuxtLink to="/">Home</NuxtLink></li>
+                    <li><NuxtLink to="/gunplas">Gunplas</NuxtLink></li>
+                </ul>
+            </nav>
+        </header>
+
+        <div>
+            <slot/>
+        </div>
+    </div>
+</template>
---
+++ pages/gunplas.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="">
+      <h1>Abouti</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+      <div>
+      <ul>
+        <li v-for="gunpla in data" :key="gunpla.id">
+          {{ gunpla.name }}
+        </li>
+      </ul>
+    </div>
+    </div>
+  </template>
+  
+  <script setup>
+    const { data, refresh, error } = await useJsonPlaceholderData('gunplas')
+  </script>
+  
+
+
+    
+  
